e0d2fc2b450b1ef3ca0afbf2ff65f1106388a94d
Richard Kozak <kozakrichard@users.noreply.github.com>
2021-12-24 19:20:33 -0500

    images can be viewed in fullscreen, information added

--- src/Components/Painting.js
+++ src/Components/Painting.js
@@ -2,14 +2,14 @@ import React, {useState, useEffect, useRef} from 'react';
 import './Painting.css'
 import ArtGetter from "./ArtGetter.js"
 import pozzi from "../artwork/dr.pozzi_sargent.jpg"
-import clickAgain from "../artwork/clickAgain.JPG"
+import clickAgain2 from "../artwork/clickAgain2.JPG"
 import axios from 'axios';
 
 
 function Painting(props) {
     var [painting, setPainting] = useState(pozzi);
     const [data] = ArtGetter("https://collectionapi.metmuseum.org/public/collection/v1/objects/" + props.num)
-    
+    const [fullScreen, setFullScreen] = useState("none");
 
     useEffect(() => {
         // GET request using axios inside useEffect React hook
@@ -21,10 +21,9 @@ function Painting(props) {
 
     return (
         <div className = "artwork-display">
-            {/*<div className = "get-btn">
-                <button onClick = {() => setPainting(data)}>Show Here</button>
-                           
-            </div>*/}
+            <div className = "get-btn">
+                <button onClick = {() => setFullScreen("block")}>Expand</button>           
+            </div>
             <div className = "painting-data">
                 {/*<p style = {{fontStyle: "italic"}}>{painting.title}</p>
                 <p>{painting.artistDisplayName}</p>
@@ -32,16 +31,31 @@ function Painting(props) {
                 {console.log(painting)}*/}
                 <div>
                     <img className ="art-now" 
-                        src = {painting.primaryImage || clickAgain} 
+                        src = {painting.primaryImage || clickAgain2} 
                         onClick = {() => setPainting(data) }
                         class="thumbnail" 
                         alt = "painting"
                         title = "Click to Change"
                     />
                 </div>
-                {/*<p>{painting.medium}</p>
-                <p>Made {painting.objectDate}</p>
-                <a href = {painting.objectURL} target = "_blank" rel="noopener noreferrer">Open this work on the Met's website</a>*/}
+
+                
+                <div className = "fullScreen" style = {{display : fullScreen}}>
+                    <button className = "close-img" onClick = {() => setFullScreen("none")}>X</button>
+                    <img className = "fullScreenImg"
+                    src = {painting.primaryImage}
+                    alt = ""
+                    />
+                    <div className = "caption">
+                        <p>{painting.artistDisplayName}</p>
+                        <p style = {{fontStyle : "italic"}}>{painting.title}</p>
+                        <p>Made {painting.objectDate}</p>
+                        <p>{painting.medium}</p>
+                        <a style = {{color : "blue"}} href = {painting.objectURL} target = "_blank" rel="noopener noreferrer">Open this work on the Met's website</a>
+                    </div>
+                    
+                </div>
+                
             </div>
             
         </div>
